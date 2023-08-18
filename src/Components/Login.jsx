1407b79831e8016029719af146d1d94ac0b60c37
Asad Awan <asadawn3@gmail.com>
2023-08-18 18:34:12 +0500

    login form validation done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,20 +1,89 @@
-import React,{useState} from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
 const Login = () => {
-  const[userFname,setUserFname]=useState('')
-  const[password,setPassword]=useState('')
+  const validationSchema = Yup.object().shape({
+    username: Yup.string()
+      .required("Username is required")
+      .min(6, "Username must be at least 6 characters")
+      .max(20, "Username must not exceed 20 characters"),
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(40, "Password must not exceed 40 characters"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   return (
-    
-    <div className='container-app'>
-      <div className='form-content mt-4'>
-        <h2 className='text-light'>User Information</h2>
-        <input type="text" className='form-control mb-2' onChange={(e)=>{setUserFname(e.target.value)}} value={userFname} placeholder='Email/UserName' required /> 
-        <input type="password" className='form-control mb-2' onChange={(e)=>{setPassword(e.target.value)}} value={password} placeholder='Password' required />
-        <button className='btn btn-primary'>Login</button>
-      </div>
+    <div className="container-app">
+      <form className="col-3" onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="text-light">User Information</h2>
+        <div className="form-group m-2">
+          <input
+            type="text"
+            autoComplete="off"
+            placeholder="Username"
+            {...register("username")}
+            className={`form-control ${errors.username ? "is-invalid" : ""}`}
+          />
+          {errors.username && (
+            <div className="invalid-feedback">{errors.username.message}</div>
+          )}
+        </div>
+        <div className="form-group m-2">
+          <input
+            type="email"
+            autoComplete="off"
+            placeholder="Email"
+            {...register("email")}
+            className={`form-control ${errors.email ? "is-invalid" : ""}`}
+          />
+          {errors.email && (
+            <div className="invalid-feedback">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="form-group m-2">
+          <input
+            type="password"
+            autoComplete="off"
+            placeholder="Password"
+            {...register("password")}
+            className={`form-control ${errors.password ? "is-invalid" : ""}`}
+          />
+          {errors.password && (
+            <div className="invalid-feedback">{errors.password.message}</div>
+          )}
+        </div>
+        <div className="form-group m-2">
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+          <button
+            type="button"
+            onClick={reset}
+            className="btn btn-warning float-right m-2"
+          >
+            Reset
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-
-export default Login
+export default Login;
